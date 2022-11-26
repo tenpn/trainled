0948ec14f420636563ae2c48306925755d6983eb
Andrew Fray <andrew.fray@gmail.com>
2022-11-26 21:18:09 +0000

    parameterising some queries

--- trains.py
+++ trains.py
@@ -16,15 +16,18 @@ def load_secrets() -> Dict[str,str]:
         }
 
 SECRETS = load_secrets()
+URL = "https://huxley2.azurewebsites.net"
 
-auth_str = f"?accessToken={SECRETS['access_key']}"
-right_from_left_query = f"https://huxley2.azurewebsites.net/arrivals/{SECRETS['right_stn']}/from/{SECRETS['left_stn']}/2{auth_str}"
+def query(path: str) -> str:
+    return f"{URL}/{path}?accessToken={SECRETS['access_key']}"
+
+right_from_left_query = query(f"arrivals/{SECRETS['right_stn']}/from/{SECRETS['left_stn']}/2")
 print(right_from_left_query)
 right_from_left = requests.get(right_from_left_query)
 trains = right_from_left.json().get("trainServices")
 for train in trains:
     print(f"train {train['serviceIdUrlSafe']} from {train['origin'][0]['locationName']} to {train['destination'][0]['locationName']}")
-    train_info = requests.get(f"https://huxley2.azurewebsites.net/service/{train['serviceIdUrlSafe']}?{auth_str}").json()
+    train_info = requests.get(query(f"service/{train['serviceIdUrlSafe']}")).json()
     print(f'due to arrive at {train_info["sta"]}')
     
 
