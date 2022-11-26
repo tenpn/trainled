2130d16ae03a47f3423b90232958eedc70d6b69e
Andrew Fray <andrew.fray@gmail.com>
2022-11-26 21:05:44 +0000

    hiding train stations in secrets
    so I'm not exposing my location in the future

--- trains.py
+++ trains.py
@@ -1,13 +1,27 @@
 import requests
+from typing import Dict
 
-def load_auth() -> str:
+def load_secrets() -> Dict[str,str]:
+    """just uses newlines to grab info and assumes ordering
+
+    Returns:
+        Dict[str,str]: the secrets data, with useful keys
+    """
     with open("secrets.txt", "r") as secrets:
-        return f"accessToken={secrets.readline()}"
+        lines = secrets.readlines()
+        return { 
+            "access_key": lines[0].strip(),
+            "left_stn": lines[1].strip(),
+            "right_stn": lines[2].strip(),
+        }
 
-auth_str = load_auth()
-knn_from_bhm = requests.get(
-    "https://huxley2.azurewebsites.net/arrivals/knn/from/bhm/2?" + auth_str)
-trains = knn_from_bhm.json().get("trainServices")
+SECRETS = load_secrets()
+
+auth_str = f"?accessToken={SECRETS['access_key']}"
+right_from_left_query = f"https://huxley2.azurewebsites.net/arrivals/{SECRETS['right_stn']}/from/{SECRETS['left_stn']}/2{auth_str}"
+print(right_from_left_query)
+right_from_left = requests.get(right_from_left_query)
+trains = right_from_left.json().get("trainServices")
 for train in trains:
     print(f"train {train['serviceIdUrlSafe']} from {train['origin'][0]['locationName']} to {train['destination'][0]['locationName']}")
     train_info = requests.get(f"https://huxley2.azurewebsites.net/service/{train['serviceIdUrlSafe']}?{auth_str}").json()
