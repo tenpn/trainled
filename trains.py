3f9f5dd4b4692723f15a092c0fb49c3a2eb621ad
Andrew Fray <andrew.fray@gmail.com>
2022-12-04 22:03:23 +0000

    left and right trains!!!

--- trains.py
+++ trains.py
@@ -42,11 +42,12 @@ def hours_decimal_from_time_str(time_str: str) -> float:
     # not sure if this is 24h clock or not yet?
     return int(time_str[0:2]) + (int(time_str[3:])/60.0)
 
-def get_locations_from_train_info(train_info: Dict) -> Dict[str, Union[str, float]]:
+def get_locations_from_train_info(train_info: Dict, start_crs: str) -> Dict[str, Union[str, float]]:
     """generates info about when this train will be visiting each station 
 
     Args:
         train_info (Dict): from huxley/service endpoint
+        start_crs (str): what station are we starting at?
 
     Returns:
         Dict[str, Union[str, float]]: { crs: "crs code", time: decimal-hours-when-train-is-due }
@@ -56,7 +57,7 @@ def get_locations_from_train_info(train_info: Dict) -> Dict[str, Union[str, floa
     prev_locations = train_info["previousCallingPoints"][0]["callingPoint"]
     interesting_locations = []
     for prev_location in prev_locations:
-        if len(interesting_locations) > 0 or prev_location["crs"].lower() == SECRETS["left_stn"].lower():
+        if len(interesting_locations) > 0 or prev_location["crs"].lower() == start_crs.lower():
             interesting_locations.append({
                 'crs': prev_location['crs'],
                 'time': hours_decimal_from_time_str(prev_location['st']),
@@ -75,7 +76,7 @@ def get_train_position_from_station_times(train_times_at_stations: List[float], 
         now (float): current time in decimal-hours
 
     Returns:
-        Tuple[int, float]: (the index of our next station, the proportional distance between last station and next)
+        Tuple[int, float]: (the index of our prev station, the proportional distance between prev station and next)
     """
     for stn_index in range(1, len(train_times_at_stations)):
         prev_stn_time = train_times_at_stations[stn_index-1]
@@ -113,30 +114,59 @@ def make_ascii_tracks(station_chars: List[str], station_separations: List[float]
     return (track_str, station_indicies)
 
 if __name__=="__main__":
+    distances = load_distances()
 
     left_to_right = requests.get(query(f"arrivals/{SECRETS['right_stn']}/from/{SECRETS['left_stn']}"))
 
-    lr_train_infos = [requests.get(query(f"service/{train['serviceIdUrlSafe']}")).json() for train in left_to_right.json().get("trainServices")]
-    lr_train_locs = [get_locations_from_train_info(train_info) for train_info in lr_train_infos]
-    print("\n".join([str(train_loc) for train_loc in lr_train_locs]))
+    lr_train_infos = [requests.get(query(f"service/{train['serviceIdUrlSafe']}")).json() 
+                      for train in left_to_right.json().get("trainServices")]
+    lr_train_locs = [get_locations_from_train_info(train_info, SECRETS["left_stn"]) for train_info in lr_train_infos]
+    
+    right_to_left = requests.get(query(f"arrivals/{SECRETS['left_stn']}/from/{SECRETS['right_stn']}"))
+    rl_train_infos = [requests.get(query(f"service/{train['serviceIdUrlSafe']}")).json()
+                      for train in right_to_left.json().get("trainServices")]
+    rl_train_locs = [get_locations_from_train_info(train_info, SECRETS["right_stn"]) for train_info in rl_train_infos]
 
+    
     # there's something wrong with this format?
     # now = datetime.strptime("2022-12-03T20:53:08.2488111+00:00".strip(), "%Y-%m-%dT%H:%M:%S.%f%z")
     # hey let's be dumb:
     now = hours_decimal_from_time_str(lr_train_infos[0]['generatedAt'][11:16])
     print(now)
+    
+    print(">")
+    print("\n".join([str(train_loc)
+          for train_loc in lr_train_locs if train_loc[0]['time'] <= now]))
 
-    lr_train_positions = [get_train_position_from_station_times([stn['time'] for stn in train_loc], now) for train_loc in lr_train_locs]
-    lr_train_positions = [train_pos for train_pos in lr_train_positions if train_pos is not None]
-
+    print("<")
+    print("\n".join([str(train_loc)
+          for train_loc in rl_train_locs if train_loc[0]['time'] <= now]))
+    
     # doesn't matter which train we use, stations are the same
     station_chars = [stn['crs'][0] for stn in lr_train_locs[0]]
-    distances = load_distances()
     (tracks_str, station_indicies) = make_ascii_tracks(station_chars, distances)
+
+    lr_train_positions = [get_train_position_from_station_times([stn['time'] for stn in train_loc], now) 
+                          for train_loc in lr_train_locs]
+    lr_train_positions = [train_pos for train_pos in lr_train_positions if train_pos is not None]
+    
+    rl_train_positions = [get_train_position_from_station_times([stn['time'] for stn in train_loc], now)
+                          for train_loc in rl_train_locs]
+    rl_train_positions = [train_pos for train_pos in rl_train_positions if train_pos is not None]
+    # need to flip indicies for the other direction
+    rl_train_positions = [(len(station_indicies) - prev_stn_index-1, 1-prop) for (prev_stn_index,prop) in rl_train_positions]
+    print(rl_train_positions)
+
 
     for (prev_stn_index, prop) in lr_train_positions:
         station_interval = station_indicies[prev_stn_index + 1] - station_indicies[prev_stn_index]
         train_char_index = station_indicies[prev_stn_index] + math.floor(prop * station_interval)
         tracks_str = tracks_str[:train_char_index] + '>' + tracks_str[train_char_index+1:]
+    
+    for (prev_stn_index, prop) in rl_train_positions:
+        # we're going left
+        station_interval = station_indicies[prev_stn_index] - station_indicies[prev_stn_index - 1]
+        train_char_index = station_indicies[prev_stn_index - 1] + math.floor(prop * station_interval)
+        tracks_str = tracks_str[:train_char_index] + '<' + tracks_str[train_char_index+1:]
 
     print(tracks_str)
